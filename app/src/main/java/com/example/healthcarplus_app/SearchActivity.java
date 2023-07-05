341498cf37907b89ad99b5c3762d381649b2a4bf
Reem Aqra <101292078+ReemAqra@users.noreply.github.com>
2023-07-05 19:15:44 +0300

    Navigation bar admin(2) +Verification(12) + admin/customer?(1)

--- app/src/main/java/com/example/healthcarplus_app/SearchActivity.java
+++ app/src/main/java/com/example/healthcarplus_app/SearchActivity.java
@@ -7,22 +7,24 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity3_admin extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity3_admin);
+        setContentView(R.layout.activity_search);
         // ----------------------------- Navigation Section ------------------------------
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationId);
         bottomNavigationView.setSelectedItemId(R.id.home);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home_button) {
-                return true;
-            } else if (item.getItemId() == R.id.Search_button) {
-                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity3_admin.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                 finish();
+                return true;
+            } else if (item.getItemId() == R.id.Search_button) {
+
                 return true;
             } else if (item.getItemId() == R.id.sell_button) {
             } else if (item.getItemId() == R.id.cost_button) {
@@ -30,7 +32,5 @@ public class MainActivity3_admin extends AppCompatActivity {
             return false;
         });
         // ----------------------------------------------------------------------------
-
-
     }
 }
